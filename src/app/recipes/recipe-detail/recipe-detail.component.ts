728243f6a4509698f8c3e0a1427c1a3d2712c2df
Adán González Rodríguez <adan.gonzalez@clickdimensions.com>
2023-08-16 20:08:38 +0100

    Programmatic Navigation to the Edit Page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit} from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Route, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model'
 import { RecipeService } from '../recipe.service';
@@ -11,26 +11,34 @@ import { RecipeService } from '../recipe.service';
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number
-  
+
   constructor(
     private recipeService: RecipeService,
+    private router: Router,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     // Snapshot is static
     // const id = this.route.snapshot.params['id'] 
-    
+
     this.route.params
       .subscribe(
         (params: Params) => {
           this.id = +params['id']; // adding plus to convert string to number
           this.recipe = this.recipeService.getRecipe(this.id);
         }
-    )
+      )
   }
-  
+
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(["edit"], { relativeTo: this.route });
+
+    // Just to practice
+    // this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route });
   }
 }
