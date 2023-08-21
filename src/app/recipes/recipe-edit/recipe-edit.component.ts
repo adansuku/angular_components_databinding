e74d2a625118295585b1daabebaa4f742185dfc2
Adán González Rodríguez <adan.gonzalez@clickdimensions.com>
2023-08-21 08:56:13 +0100

    Creating the Form For Editing Recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,9 +12,11 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit, OnDestroy {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
   ) { }
 
   ngOnInit(): void {
@@ -20,12 +25,36 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
         (params: Params) => {
           this.id = +params['id']
           this.editMode = params['id'] != null;
-          console.log(this.editMode)
+          this.initForm();
         }
       )
   }
 
+  private initForm() {
+    let recipeName = ''
+    let recipeImagePath = ''
+    let recipeDescription = ''
+    let recipeIngredients = []
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      recipeIngredients = recipe.ingredients;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription),
+    });
+
+  }
+
   ngOnDestroy(): void {
   }
+
+
 
 }
