d6d0f9836b5883f110d8f82b238cadcf8a79e64b
Adán González Rodríguez <adan.gonzalez@clickdimensions.com>
2023-08-22 12:54:21 +0100

    Adding the Auth Page

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipeStartComponent } from "./app/recipes/recipe-start/recipe-start.co
 import { RecipeDetailComponent } from "./app/recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./app/recipes/recipe-edit/recipe-edit.component";
 import { RecipeResolverService } from "./app/recipes/recipe-resolver.service";
+import { AuthComponent } from "./app/auth/auth/auth.component";
 
 const appRoutes: Routes = [
 	{
@@ -27,6 +28,7 @@ const appRoutes: Routes = [
 		path: 'shopping-list',
 		component: ShoppingListComponent
 	},
+	{ path: 'auth', component: AuthComponent }
 
 ]
 
